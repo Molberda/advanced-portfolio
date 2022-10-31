e3b3b557598f2c838a775a128db309e3fb5de361
David <davidmollera@hotmail.com>
2022-10-31 13:27:42 -0500

    set up the javaScript function to move the shapes 'onmousemove' over the body tag

--- app.js
+++ app.js
@@ -3,6 +3,19 @@
 // pP19DnVESUmrE9Psq
 let isModalOpen = false
 let darkToggle = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll('.shape')
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+
+    for (let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1: 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 
 function toggleDark(){
