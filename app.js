7259bb94a163ee4379e7bd442a3faa7e4c04a4e7
David <davidmollera@hotmail.com>
2022-10-31 15:15:38 -0500

    added a rot const that controls the rotation of shapes, added the rotation transformation to the end of the position transformation, also added position relative to the footer in order to prevent the shapes from overlapping it

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
 // pP19DnVESUmrE9Psq
 let isModalOpen = false
 let darkToggle = false
-const scaleFactor = 1 / 20
+const scaleFactor = 1 / 10
 
 function moveBackground(event){
     const shapes = document.querySelectorAll('.shape')
     const x = event.clientX * scaleFactor
     const y = event.clientY * scaleFactor
-
+    const rot = event.clientX / 2
+    
     for (let i = 0; i < shapes.length; ++i){
         const isOdd = i % 2 !== 0
         const boolInt = isOdd ? -1: 1
-        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${rot}deg)`   
     }
 }
-
 
 function toggleDark(){
     darkToggle = !darkToggle
